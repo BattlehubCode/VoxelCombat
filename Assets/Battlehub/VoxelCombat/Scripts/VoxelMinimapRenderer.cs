af37be36863e596ac5e917c46d3576eb919951bd
Battlehub0x <Vadim.Andriyanov@outlook.com>
2018-04-04 23:56:34 +0300

    VoxelMimimapRenderer background layer rendering completed

--- Assets/Battlehub/VoxelCombat/Scripts/VoxelMinimapRenderer.cs
+++ Assets/Battlehub/VoxelCombat/Scripts/VoxelMinimapRenderer.cs
@@ -27,6 +27,9 @@ namespace Battlehub.VoxelCombat
     {
         public event EventHandler Loaded;
 
+        [SerializeField]
+        private int m_desiredResolution = 512;
+
         private Texture2D m_bgTexture;
         private Texture2D m_fgTexture;
         private IVoxelMap m_voxelMap;
@@ -91,15 +94,48 @@ namespace Battlehub.VoxelCombat
 
         private void CreateTextures()
         {
-            CalculateStaticMapHeight();
-
             MapRect bounds = m_voxelMap.MapBounds;
+
             int size = m_voxelMap.Map.GetMapSizeWith(0);
+           
+            int toZeroWeight = (1 << GameConstants.MinVoxelActorWeight);
+            bounds.ColsCount *= toZeroWeight;
+            bounds.RowsCount *= toZeroWeight;
+            bounds.Col *= toZeroWeight;
+            bounds.Row *= toZeroWeight;
+
+            float centerRow = bounds.Row + bounds.RowsCount / 2.0f;
+            float centerCol = bounds.Col + bounds.ColsCount / 2.0f;
+            int boundsSize = Mathf.Max(bounds.ColsCount, bounds.RowsCount);
+
+            Vector2 p0 = new Vector2(Mathf.Max(0, centerRow - boundsSize / 2.0f), Mathf.Max(0, centerCol - boundsSize / 2.0f));
+            Vector2 p1 = new Vector2(Mathf.Min(size, centerRow + boundsSize / 2.0f), Mathf.Min(size, centerCol + boundsSize / 2.0f));
+
+            p0.x = Mathf.FloorToInt((p0.x) / toZeroWeight) * toZeroWeight;
+            p0.y = Mathf.FloorToInt((p0.y) / toZeroWeight) * toZeroWeight;
+
+            p1.x = Mathf.CeilToInt((p1.x) / toZeroWeight) * toZeroWeight;
+            p1.y = Mathf.CeilToInt((p1.y) / toZeroWeight) * toZeroWeight;
+
+            bounds.Row = (int)p0.x;
+            bounds.Col = (int)p0.y;
+
+            bounds.RowsCount = (int)(p1.x - p0.x);
+            bounds.ColsCount = (int)(p1.y - p0.y);
+            boundsSize = Mathf.Max(bounds.ColsCount, bounds.RowsCount);
+            bounds.RowsCount = boundsSize;
+            bounds.ColsCount = boundsSize;
+
+            size = boundsSize;
+            int scale = Mathf.Max(m_desiredResolution / size, 1);
+            size *= scale;
+
+            CalculateStaticMapHeight();
             m_bgTexture = CreateTexture(size, m_skyColor);
             m_fgTexture = CreateTexture(size, new Color(1, 1, 1, 0));
 
-            Draw(m_voxelMap.Map.Root, new Coordinate(0, 0, 0, m_voxelMap.Map.Weight), bounds, m_bgTexture, cell => cell.VoxelData.GetLastStatic(), data => m_groundBaseColor);
-            Draw(m_voxelMap.Map.Root, new Coordinate(0, 0, 0, m_voxelMap.Map.Weight), bounds, m_fgTexture, cell =>
+            Draw(m_voxelMap.Map.Root, new Coordinate(0, 0, 0, m_voxelMap.Map.Weight), bounds, m_bgTexture, scale, cell => cell.VoxelData.GetLastStatic(), data => m_groundBaseColor);
+            Draw(m_voxelMap.Map.Root, new Coordinate(0, 0, 0, m_voxelMap.Map.Weight), bounds, m_fgTexture, scale, cell =>
             {
                 VoxelData last = cell.VoxelData.GetLast();
                 if(last != null && !VoxelData.IsStatic(last.Type))
@@ -146,9 +182,35 @@ namespace Battlehub.VoxelCombat
             });
         }
 
-        private void Draw(MapCell cell, Coordinate coord, MapRect bounds, Texture2D texture, Func<MapCell, VoxelData> voxelDataSelector, Func<VoxelData, Color> colorSelector)
+        private void Draw(MapCell cell, Coordinate coord, MapRect bounds, Texture2D texture, int scale, Func<MapCell, VoxelData> voxelDataSelector, Func<VoxelData, Color> colorSelector)
         {
-            //Coordinate boundsWeightCoord = coord.ToWeight(GameConstants.MinVoxelActorWeight);
+            Coordinate zeroCoord = coord.ToWeight(0);
+            MapPos p0 = zeroCoord.MapPos;
+            int size = (1 << coord.Weight);
+            MapPos p1 = p0;
+            p1.Add(size, size);
+
+            p0.Row = Mathf.Max(p0.Row, bounds.Row);
+            p0.Col = Math.Max(p0.Col, bounds.Col);
+            p1.Row = Mathf.Min(p1.Row, bounds.Row + bounds.RowsCount);
+            p1.Col = Mathf.Min(p1.Col, bounds.Col + bounds.ColsCount);
+
+            int cols = p1.Col - p0.Col;
+            int rows = p1.Row - p0.Row; 
+            if(cols <= 0 || rows <= 0)
+            {
+                return;
+            }
+            
+            cols *= scale;
+            rows *= scale;
+
+            p0.Row -= bounds.Row;
+            p0.Col -= bounds.Col;
+
+            p0.Row *= scale;
+            p0.Col *= scale;
+
             VoxelData data = null;
             if (cell.VoxelData != null)
             {
@@ -157,82 +219,34 @@ namespace Battlehub.VoxelCombat
 
             if (data != null)
             {
-                Coordinate zeroWeightCoord = coord.ToWeight(0);
-                int size = 1 << coord.Weight;
                 float height = data.Altitude + data.Height;
                 float deltaColor = (1.0f - (height / m_staticMapHeight)) * 0.1f;
                 Color color = colorSelector(data) - new Color(deltaColor, deltaColor, deltaColor, 0);
-                for (int x = 0; x < size; ++x)
+                for (int r = 0; r < rows; ++r)
                 {
-                    for (int y = 0; y < size; ++y)
+                    for (int c = 0; c < cols; ++c)
                     {
-                        texture.SetPixel(zeroWeightCoord.Col + y, zeroWeightCoord.Row + x, color);
+                        texture.SetPixel(p0.Col + c, p0.Row + r, color);
                     }
                 }
             }
 
-            if (cell.Children != null && cell.Children.Length > 0)
+            if (cell.Children != null && cell.Children.Length > 0 && coord.Weight > GameConstants.MinVoxelActorWeight)
             {
                 coord.Weight--;
                 coord.Col *= 2;
                 coord.Row *= 2;
+
                 for (int i = 0; i < 4; i++)
                 {
                     Coordinate childCoord = coord;
                     childCoord.Row += i / 2;
                     childCoord.Col += i % 2;
 
-                    Draw(cell.Children[i], childCoord, bounds, texture, voxelDataSelector, colorSelector);
+                    Draw(cell.Children[i], childCoord, bounds, texture, scale, voxelDataSelector, colorSelector);
                 }
             }
 
         }
-
-        /*
-        private void DrawGround(MapCell cell, Coordinate coord, MapRect bounds)
-        {
-            //Coordinate boundsWeightCoord = coord.ToWeight(GameConstants.MinVoxelActorWeight);
-            //if(!bounds.Contains(boundsWeightCoord.MapPos))
-            //{
-            //    return;
-            //}
-
-            VoxelData lastStatic = null;
-            if(cell.VoxelData != null)
-            {
-                lastStatic = cell.VoxelData.GetLastStatic();
-            }
-
-            if(lastStatic != null)
-            {
-                Coordinate zeroWeightCoord = coord.ToWeight(0);
-                int size = 1 << coord.Weight;
-                float height = lastStatic.Altitude + lastStatic.Height;
-                float deltaColor = (1.0f - (height / m_staticMapHeight)) * 0.1f;
-                Color color = m_groundBaseColor - new Color(deltaColor, deltaColor, deltaColor, 0);
-                for (int x = 0; x < size; ++x)
-                {
-                    for (int y = 0; y < size; ++y)
-                    {    
-                        m_bgTexture.SetPixel(zeroWeightCoord.Col + y, zeroWeightCoord.Row + x, color);
-                    }
-                }
-            }
-
-            if (cell.Children != null && cell.Children.Length > 0)
-            {
-                coord.Weight--;
-                coord.Col *= 2;
-                coord.Row *= 2;
-                for (int i = 0; i < 4; i++)
-                {
-                    Coordinate childCoord = coord;
-                    childCoord.Row += i / 2;
-                    childCoord.Col += i % 2;
-
-                    DrawGround(cell.Children[i], childCoord, bounds);
-                }
-            }
-        }*/
     }
 }
